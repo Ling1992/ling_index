d485597a1936e4bf68318fc41d7e40478336395c
Ling1992 <l_ing1992@163.com>
2017-08-19 17:32:06 +0800

    image cache

--- app/Http/Controllers/Index/ImageController.php
+++ app/Http/Controllers/Index/ImageController.php
@@ -10,15 +10,26 @@ namespace App\Http\Controllers\Index;
 
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+//use Illuminate\Support\Facades\Log;
 
 class ImageController
 {
     function image(Request $request){
         $url = $request->input("url");
 
-        Log::info('ling', [$url]);
-
-        return response(file_get_contents($url))->header("Content-Type", "image/jpeg");
+//        Log::info('ling', [$request->server()]);
+        if ( $request->server("HTTP_IF_MODIFIED_SINCE") ){
+//            Log::info('ling',['last_time'=>strtotime(preg_replace('/;.*$/', '', $_SERVER['HTTP_IF_MODIFIED_SINCE'])),'now'=>time()]);
+            if (time() <= strtotime(preg_replace('/;.*$/', '', $_SERVER['HTTP_IF_MODIFIED_SINCE'])))
+                return response('',304)->header('LAST-MODIFIED',$request->server("HTTP_IF_MODIFIED_SINCE"));
+        }
+//        Log::info('ling', [$url]);
+        return response(file_get_contents($url))->withHeaders([
+            "Cache-Control"=>"private, max-age=10800, pre-check=10800",
+            "Pragma"=>"private",
+            "Expires"=>date(DATE_RFC822, strtotime(" 2 day")),
+            "Last-Modified"=>date(DATE_RFC822, strtotime(" 2 day")),
+            "Content-type"=>"image/jpeg",
+        ]);
     }
 }

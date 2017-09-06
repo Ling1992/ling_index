464b85d08245aaf7a7f35921813965aa596a7e11
Ling1992 <l_ing1992@163.com>
2017-09-06 14:03:47 +0800

    update footer

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
-        <p style="color: #ffffff;margin: 10px 0 10px 0 " > 本站以分享各类网络文章文章为主，为你带来简洁舒适的阅读体验，为你带来简洁舒适的阅读体验</p>
-        <p> 提示 ：所有文章来源于网络, 如果有版权问题请与<button class="btn-link" type="button" onclick="contactUs()">我们</button>联系, 我们将及时删除！</p>
+        <p style="color: #ffffff;margin: 10px 0 10px 0; text-align: center" > 本站以分享各类网络文章文章为主，为你带来简洁舒适的阅读体验</p>
+        <p style="text-align: right"> 提示 ：所有文章来源于网络, 如果有版权问题请与<button class="btn-link" type="button" onclick="contactUs()">我们</button>联系, 我们将及时删除！</p>
     </div>
 </footer>
 <script>

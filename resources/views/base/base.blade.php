0ae85512d928156d8716c5fd28c1e621cf25689f
Ling1992 <l_ing1992@163.com>
2017-09-08 11:19:05 +0800

    360 收录

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- 站长 -->
     <meta name="baidu-site-verification" content="xXN4NvYoFW" />
+    <!-- 360 -->
+    <meta name="360-site-verification" content="4f775ffdfd2567e54ad13073cb07369e" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -39,6 +41,8 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.css"/>
 
     <style type="text/css">
         a:link {
@@ -83,12 +87,29 @@
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="{{ asset('bootstrap/assets/js/ie10-viewport-bug-workaround.js') }}"></script>
 <script src="{{ asset('js/echo.min.js') }}"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.js"></script>
+
 <script>
     echo.init({
         offset: 0,
         throttle: 2
     });
 </script>
+
+<script type="text/javascript">
+    $(function(){
+        $('.slick').slick({
+            dots: false,
+            lazyLoad: 'ondemand',
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            arrows:false
+        });
+    });
+</script>
+
 <!-- 百度统计 -->
 <script>
     var _hmt = _hmt || [];
@@ -97,6 +118,13 @@
         hm.src = "https://hm.baidu.com/hm.js?94e8b3a588e30f9d6ae660cdc60731c3";
         var s = document.getElementsByTagName("script")[0];
         s.parentNode.insertBefore(hm, s);
+    })();
+</script>
+
+<!-- 360 主动收录 -->
+<script>(function(){
+        var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?3c0038ae1f5880a0a591b308f53843d6":"https://jspassport.ssl.qhimg.com/11.0.1.js?3c0038ae1f5880a0a591b308f53843d6";
+        document.write('<script src="' + src + '" id="sozz"><\/script>');
     })();
 </script>
 

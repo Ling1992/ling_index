f0715bcd3ce6535d2c80109f6469d8df2b0fd085
Ling1992 <l_ing1992@163.com>
2017-10-09 17:10:21 +0800

    wx auto response

--- app/Http/Controllers/Wx/IndexController.php
+++ app/Http/Controllers/Wx/IndexController.php
@@ -8,6 +8,7 @@
 namespace App\Http\Controllers\Wx;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class IndexController extends Controller
 {
@@ -18,6 +19,39 @@ class IndexController extends Controller
     }
 
     function respondMSG(){
+        $postStr = file_get_contents("php://input", 'r');
+        if (!empty($postStr)){
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
 
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+                           <ToUserName><![CDATA[%s]]></ToUserName>
+                           <FromUserName><![CDATA[%s]]></FromUserName>
+                           <CreateTime>%s</CreateTime>
+                           <MsgType><![CDATA[%s]]></MsgType>
+                           <Content><![CDATA[%s]]></Content>
+                           <FuncFlag>0</FuncFlag>
+                           </xml>";
+
+            //订阅事件
+            if ($postStr->Event == "subscribe") {
+
+                $msgType = "text";
+                $contentStr = "欢迎关注!!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+
+                echo $resultStr;
+            }
+
+            if (! empty($keyword)) {
+                $msgType = "text";
+                $contentStr = "自动回复 --- 》 " ;  // 判断 是否 是电影 资讯 ！！
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+            }
+        }
     }
 }
